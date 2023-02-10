90261d82aef96dc8724f2e7c0a0688835c06912a
jollySleeper <git@pm21.anonaddy.com>
2023-02-10 11:53:49 +0530

    Updated: Neovim: Darkened Sidebar

--- neovim/.config/nvim/after/plugin/theme.lua
+++ neovim/.config/nvim/after/plugin/theme.lua
@@ -18,20 +18,25 @@ local my_theme = "dark"; -- Fallback to Dark Theme
 -- Line 21 will be Written Using `sed` Command
 -- `sed -i "21s/dark/light/g" $HOME/.config/nvim/after/plugin/theme.lua`
 -- `sed -i "21s/dark/light/g" $HOME/.config/nvim/after/plugin/theme.lua`
-my_theme = "dark"; -- Automated Line
+my_theme = "light"; -- Automated Line
 
-local bg_color = "#24292e" -- Dark Theme Color
+local my_colors = {
+    alacritty_light_theme_bg = "#f3f2f1", -- Same as Alacritty
+    light_theme_bg = "#f6f8fa"
+}
+
+local bg_color = {}
 -- Setting Custom BG for Light Theme
 if my_theme == "light" then
-    bg_color = "#f3f2f1" -- Same as Alacritty
+    bg_color = {
+        bg = my_colors.light_theme_bg,
+        bg2 = my_colors.alacritty_light_theme_bg,
+        bg_sidebar = my_colors.alacritty_light_theme_bg,
+        sidebar_eob = my_colors.alacritty_light_theme_bg
+    }
 end
 
 require("github-theme").setup({
     theme_style = my_theme,
-    colors = {
-        bg =  bg_color
-    }
+    colors = bg_color
 });
-
-
-
