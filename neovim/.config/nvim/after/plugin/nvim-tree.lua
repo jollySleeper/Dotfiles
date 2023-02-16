f57bb553cbd41b4fe2aac7996e9383caf45bb85f
jollySleeper <git@pm21.anonaddy.com>
2023-02-16 09:28:14 +0530

    Updated: Neovim: Updated Nvim-Tree to Open on Startup

--- neovim/.config/nvim/after/plugin/nvim-tree.lua
+++ neovim/.config/nvim/after/plugin/nvim-tree.lua
@@ -12,3 +12,10 @@ require("nvim-tree").setup({
 
 -- To View All The Active Nvim-Tree Mappings On The Fly
 -- vim.pretty_print(require('nvim-tree.api').config.mappings.active());
+
+local function open_nvim_tree()
+  -- open the tree
+  require("nvim-tree.api").tree.toggle({ focus = false });
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
