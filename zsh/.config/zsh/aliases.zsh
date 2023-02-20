18bf90056d00f5ac28e6b77c59bb783c16f13102
jollySleeper <git@pm21.anonaddy.com>
2023-02-20 20:41:56 +0530

    Updated: Zsh: Handled Case for Ubuntu

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,10 +3,6 @@ alias exa='exa --icons'
 alias exal='exa -lag'
 alias exat='exa -T'
 
-#OpenVPN3 
-alias list-vpn='openvpn3 sessions-list'
-alias start-vpn='openvpn3 session-start --config $HOME/Vpn/mnet_techvpn.ovpn'
-alias stop-vpn='openvpn3 session-manage --config $HOME/Vpn/mnet_techvpn.ovpn --disconnect'
 
 #Tmux
 alias tmux='tmux -u -f $HOME/.config/tmux/tmux.conf'
@@ -19,5 +15,13 @@ alias gco='git branch --sort=-committerdate | fzf --header "Checkout Recent Bran
 alias tldrf='tldr --list | fzf --preview "tldr {1} --color=always" --preview-window=right,70% | xargs tldr'
 alias manf='man -k . | awk "{print $1}" | fzf --preview "man {1}" --preview-window=right,70% | xargs man'
 
-#FdFind
-alias fd='fdfind'
+#Only For Ubuntu
+if [[ $(awk -F= '/^ID=/ {print $2}' /etc/os-release 2> /dev/null | sed 's/"//g') == "ubuntu" ]]; then
+    #FdFind 
+    alias fd='fdfind'
+
+    #OpenVPN3 for Work
+    alias list-vpn='openvpn3 sessions-list'
+    alias start-vpn='openvpn3 session-start --config $HOME/Vpn/mnet_techvpn.ovpn'
+    alias stop-vpn='openvpn3 session-manage --config $HOME/Vpn/mnet_techvpn.ovpn --disconnect'
+fi
