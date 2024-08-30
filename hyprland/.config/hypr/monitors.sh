9d711d686159efab329736353610ae33b14548a0
jollySleeper <git@pm21.anonaddy.com>
2024-08-30 08:58:39 +0530

    Updated: Hyprland: Scripts: Using New Monitor

--- hyprland/.config/hypr/monitors.sh
+++ hyprland/.config/hypr/monitors.sh
@@ -10,8 +10,8 @@ if grep open /proc/acpi/button/lid/LID0/state; then
     # if check for Monitor is connected or not
     if [[ $(hyprctl monitors | grep "Monitor" | wc -l) > 1 ]]; then
         notification="${notification}\nDual Monitor Setup: External Monitor above Laptop Screen";
-        hyprctl keyword monitor "HDMI-A-1, 2560x1440@59.95100, 0x0, 1";
-        hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x1440, 1";
+        hyprctl keyword monitor "HDMI-A-1, 1920x1080@71.91, 0x0, 1";
+        hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x1080, 1";
     else
         notification+="${notification}\nDefault Screen Laptop Screen";
         hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x0, 1";
@@ -20,11 +20,13 @@ else
     notification="${notification}󰛧  Lid Closed";
     # Checking for Number of Displays
     if [[ $(hyprctl monitors | grep "Monitor" | wc -l) > 1 ]]; then
-        hyprctl keyword monitor "HDMI-A-1, 2560x1440@59.95100, 0x0, 1";
-        notification="${notification}\nDisabling Laptop Screen\nUsing External Monitor";
+        hyprctl keyword monitor "HDMI-A-1, 1920x1080@71.91, 0x0, 1";
+        notification="${notification}\nDisabling Laptop Screen";
+        notification="${notification}\nUsing External Monitor";
         hyprctl keyword monitor "eDP-1, disable";
     else
-        if [[ $(hyprctl monitors | grep "Monitor" | wc -l) -eq 1 ]] &&
+        if 
+            [[ $(hyprctl monitors | grep "Monitor" | wc -l) -eq 1 ]] &&
             [[ -z $(hyprctl monitors | grep "Monitor HDMI-A-1") ]]; 
         then
             hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x0, 1";
