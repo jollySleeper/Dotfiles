218659d415f6e0a0222ee751757edc332c96b5a2
jollySleeper <git@pm21.anonaddy.com>
2024-08-31 11:59:00 +0530

    Updated: Hyprland: Added Script for Random Wallpapers

---
+++ hyprland/.config/hypr/wallpaper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+send_notification() {
+    notify-send -i "/usr/share/icons/Papirus/64x64/apps/com.github.peteruithoven.resizer.svg" "Wallpaper" "$*"
+}
+
+WALLPAPER="$HOME/.cache/wallpaper/image.png"
+RANDOM_WALLPAPER=$(ls $HOME/Pictures/Wallpapers/*png | shuf -n 1)
+
+# For SwayLock
+cp -v ${RANDOM_WALLPAPER} ${WALLPAPER}
+
+send_notification "Changing Wallpaper"
+
+# WBG
+# Experimented with wbg 
+# Not using it as it is a hassle to install when swaybg is in repo
+# wbg ${WALLPAPER}
+
+# swww
+# swww init
+# swww img ${WALLPAPER} --transition-type center
+
+# SwayBG
+swaybg --mode fill --image ${WALLPAPER}
