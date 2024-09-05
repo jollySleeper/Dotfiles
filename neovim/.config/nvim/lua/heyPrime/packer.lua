76aa5710cc6cb5a6a4650ceb26eb81d525aae56d
jollySleeper <git@pm21.anonaddy.com>
2024-09-05 09:58:33 +0530

    Updated: Neovim: Using Nvim JDTLS Plugin

--- neovim/.config/nvim/lua/heyPrime/packer.lua
+++ neovim/.config/nvim/lua/heyPrime/packer.lua
@@ -93,4 +93,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     });
+
+    -- For Java
+    use('mfussenegger/nvim-jdtls');
 end);
