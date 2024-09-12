59b5634edfbfd5c80b0aeb8d8b9b81931b2f25ac
jollySleeper <git@pm21.anonaddy.com>
2024-09-12 09:11:08 +0530

    Updated: Neovim: Added TODO for Trying Lazy Nvim

--- neovim/.config/nvim/lua/heyPrime/packer.lua
+++ neovim/.config/nvim/lua/heyPrime/packer.lua
@@ -1,4 +1,5 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- TODO: Try Lazy.nvim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]];
